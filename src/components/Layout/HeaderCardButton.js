d41dbe6eb48d7abefe036ab9ac6c965e84df29d6
d.mittag <dong.mittag@gmail.com>
2022-02-10 15:01:55 +0100

    Day 2 - 4 Final push. Need fixes => Number isn`t showing up. NaN!

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 
 import CardIcon from "../Cart/CardIcon";
+import CardContext from '../../store/cart-context'
 import classes from './HeaderCardButton.module.css'
 
-const HeaderCardButton = (props) =>{
-    return <button className={classes.button} onClick={props.onClick}>
+const HeaderCardButton = (props) => {
+
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+    const cartCtx = useContext(CardContext)
+    const {items} = cartCtx
+//ToDo fix reduce() : currently showing NaN
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0)
+
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return
+        }
+        setBtnIsHighlighted(true)
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false)
+        }, 300)
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items])
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
-            <CardIcon />
+            <CardIcon/>
         </span>
         <span>
             Your Cart
         </span>
         <span className={classes.badge}>
-            3
+            {numberOfCartItems}
         </span>
     </button>
 }
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,19 +1,42 @@
+import {useRef, useState} from 'react'
 import classes from './MealItemForm.module.css'
 import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
-    return(
-        <form className={classes.form}>
-            <Input label={'Amount'} input={{
-                //default props from input
-                id: 'amount_' + props.id,
-                type: 'number',
-                min: '1',
-                max: '5',
-                step: '1',
-                defaultValue: '1'
-            }} />
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef()
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmount > 5){
+            setAmountIsValid(false)
+            return
+        }
+
+        props.onAddToCart(enteredAmountNumber)
+
+    }
+
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input
+                ref={amountInputRef}
+                label={'Amount'}
+                   input={{
+                       //default props from input
+                       id: 'amount_' + props.id,
+                       type: 'number',
+                       min: '1',
+                       max: '5',
+                       step: '1',
+                       defaultValue: '1'
+                   }}/>
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
     )
 }
